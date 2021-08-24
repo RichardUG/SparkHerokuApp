794a6197f5ba6000d01e39968bbac73c6147102b
RichardUG <rsug.rsug@gmail.com>
2021-08-23 20:44:27 -0500

    JavaDoc

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -10,6 +10,13 @@ import java.io.IOException;
  */
 public class TestClient {
 
+    /**
+     * prueba de igualdad entre el contenido de lo que obtiene nuestro servicio Heroku en la consulta a Alpha
+     * ventage API y la consulta directa a Alpha ventage API, ambos siendo consultados a Facebook como es por
+     * defecto en nuestro caso.
+     * @throws IOException Controla excepciones generadas por mala conexión
+     * @throws JSONException Controla excepciones generadas por tipos JSON
+     */
     @Test
     public void PruebaIgualHerokuAlphafbVSAlphafb() throws IOException, JSONException {
         String ExpectedJSON="",site="https://sparkheroku.herokuapp.com/facadealpha?st=fb", HerokuJSON="";
@@ -19,6 +26,12 @@ public class TestClient {
         JSONAssert.assertEquals(ExpectedJSON,HerokuJSON.toString(),true);
     }
 
+    /**
+     * prueba de desigualdad entre el contenido de lo que obtiene nuestro servicio Heroku en la consulta a Alpha
+     * ventage API hacia Microsoft y la consulta directa a Alpha ventage API hacia Facebook.
+     * @throws IOException Controla excepciones generadas por mala conexión
+     * @throws JSONException Controla excepciones generadas por tipos JSON
+     */
     @Test
     public void PruebaDiferenteHerokuAlphafbVSAlphaMSTF() throws IOException, JSONException{
         String ExpectedJSON="",site="https://sparkheroku.herokuapp.com/facadealpha?st=MSTF", HerokuJSON="";
@@ -28,6 +41,12 @@ public class TestClient {
         JSONAssert.assertNotEquals(ExpectedJSON,HerokuJSON.toString(),true);
     }
 
+    /**
+     * prueba de igualdad entre el contenido de lo que obtiene nuestro servicio Heroku en la consulta a Alpha
+     * ventage API hacia Google y la consulta directa a Alpha ventage API hacia Google también.
+     * @throws IOException Controla excepciones generadas por mala conexión
+     * @throws JSONException Controla excepciones generadas por tipos JSON
+     */
     @Test
     public void PruebaIgualHerokuAlphaGOOGVSAlphaGOOG() throws IOException, JSONException{
         String ExpectedJSON="",site="https://sparkheroku.herokuapp.com/facadealpha?st=GOOG", HerokuJSON="";
@@ -38,6 +57,12 @@ public class TestClient {
         JSONAssert.assertEquals(ExpectedJSON,HerokuJSON.toString(),true);
     }
 
+    /**
+     * pprueba de igualdad entre el contenido de lo que obtiene nuestro servicio Heroku en la consulta a Iex Cloud
+     * API y la consulta directa a Iex Cloud API ambas con stock como aapl.
+     * @throws IOException Controla excepciones generadas por mala conexión
+     * @throws JSONException Controla excepciones generadas por tipos JSON
+     */
     @Test
     public void PruebaIgualHerokuIEXvsIEX() throws IOException, JSONException{
         String ExpectedJSON="",site="https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_8a6cc2e8c79a4d01a8e938fb171f1d9c", HerokuJSON="";
